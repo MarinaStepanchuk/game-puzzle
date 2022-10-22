1325d39c63b2b9b436f831065d9041684e7bc8e3
MarinaStepanchuk <mari20002@mail.ru>
2022-10-23 01:36:13 +0300

    feat: add controlls

--- Game Puzzle/index.js
+++ Game Puzzle/index.js
@@ -7,16 +7,46 @@ function addElements (div) {
         item.classList.add('item');
         item.innerHTML = matrix[i];
         div.appendChild(item);
-    }
-}
+    };
+};
 
 function fillHtml() {
-    let div = document.createElement('div');
-    div.classList.add("game");
-    document.body.appendChild(div);
-    addElements(div);
-}
+    let gameWrapper = document.createElement('div');
+    gameWrapper.classList.add("game-wrapper");
+    document.body.appendChild(gameWrapper);
+    gameWrapper.innerHTML = `
+        <div class="game-wrapper">
+            <div class = 'control-container'>
+                <button class="shuffle">Shuffle and start</button>
+                <button class="stop">Stop</button>
+                <button class="save">Save</button>
+                <button class="results">Results</button>
+                <button class="sound"></button>
+            </div>
+            <select id="size-game">
+                <option>3x3</option>
+                <option selected>4x4</option>
+                <option>5x5</option>
+                <option>6x6</option>
+                <option>7x7</option>
+                <option>8x8</option>
+            </select>
+            <div class="info">
+                <span class="moves-title">Moves: <span class="moves">0</span></span>
+                <span class="time-tittle">Time: <time class="time"></time></span>
+            </div>
+            <div class="game"></div>
+            </div>
+    `
+    let game = document.querySelector('.game')
+    addElements(game);
+};
 
-window.addEventListener('load', (e) => {
-    fillHtml()
-})
+fillHtml();
+
+const game = document.querySelector('.game');
+const cells = Array.from(game.querySelectorAll('.item'))
+
+// window.addEventListener('load', (e) => {
+
+// });
