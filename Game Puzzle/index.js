54374e246ffd6c5261a57af6a7f0398ef21d98e3
MarinaStepanchuk <mari20002@mail.ru>
2022-10-23 11:05:06 +0300

    feat: position cells

--- Game Puzzle/index.js
+++ Game Puzzle/index.js
@@ -1,11 +1,11 @@
 let columnNum = 4;
-let matrix = new Array(columnNum ** 2).fill(0).map((item,index) => index + 1);
+let combination = new Array(columnNum ** 2).fill(0).map((item,index) => index + 1);
 
 function addElements (div) {
-    for (let i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < combination.length; i++) {
         let item = document.createElement('button');
         item.classList.add('item');
-        item.innerHTML = matrix[i];
+        item.innerHTML = combination[i];
         div.appendChild(item);
     };
 };
@@ -45,7 +45,45 @@ function fillHtml() {
 fillHtml();
 
 const game = document.querySelector('.game');
-const cells = Array.from(game.querySelectorAll('.item'))
+const cells = Array.from(game.querySelectorAll('.item'));
+const shuffleButton = document.querySelector('.shuffle');
+
+let matrix = getMatrix();
+setPozitionMatrix(matrix)
+
+function getMatrix () {
+    // let arr = cells.map((item => Number(item.innerHTML)))
+    const matrix = new Array(columnNum);
+    for (let i = 0; i < columnNum; i++) {
+        matrix[i] = new Array(columnNum);
+    };
+    let numbers = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            matrix[i][j] = combination[numbers];
+            numbers++
+        }
+    }
+    return matrix
+}
+
+function setPozitionMatrix(matrix) {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix.length; x++) {
+            const value = matrix[y][x];
+            const cell = cells[value - 1];
+            setCellStyles(cell, x, y)
+        }
+    }
+}
+
+function setCellStyles(cell, x, y) {
+    const offset = 100;
+    cell.style.transform = `translate(${x * offset}%, ${y * offset}%)`
+}
+
+
+
 
 // window.addEventListener('load', (e) => {
 
