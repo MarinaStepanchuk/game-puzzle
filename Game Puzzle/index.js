b9d2164dde93bcedfacf5641a53ab46d572e8d7b
MarinaStepanchuk <mari20002@mail.ru>
2022-10-23 17:20:34 +0300

    feat:  move by the keyboard

--- Game Puzzle/index.js
+++ Game Puzzle/index.js
@@ -2,6 +2,7 @@ let columnNum = 4;
 let emtyNumber = columnNum ** 2;
 let combination = new Array(columnNum ** 2).fill(0).map((item,index) => index + 1);
 let winnCombination =  new Array(columnNum ** 2).fill(0).map((item,index) => index + 1);
+let count = 0;
 
 //-----заполнение html-------------
 
@@ -41,8 +42,8 @@ function fillHtml() {
             </div>
             <div class="game"></div>
             </div>
-    `
-    let game = document.querySelector('.game')
+    `;
+    let game = document.querySelector('.game');
     addElements(game);
 };
 
@@ -68,11 +69,11 @@ function getMatrix () {
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix.length; j++) {
             matrix[i][j] = combination[numbers];
-            numbers++
-        }
-    }
-    return matrix
-}
+            numbers++;
+        };
+    };
+    return matrix;
+};
 
 function setPozitionMatrix(matrix) {
     let y1 = 0;
@@ -90,7 +91,7 @@ function setPozitionMatrix(matrix) {
 
 function setCellStyles(cell, x, y, x1, y1) {
     const offset = 100;
-    cell.style.transform = `translate(${(x + x1) * offset}%, ${(y + y1) * offset}%)`
+    cell.style.transform = `translate(${(x + x1) * offset}%, ${(y + y1) * offset}%)`;
 }
 
 //----------перемешивание------------
@@ -105,37 +106,38 @@ function shuffleGame(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
-    }
+    };
     return array;
-}
+};
 
 //----------смещение элемента по клику------------
 
 game.addEventListener('click', (event) => {
     const cellClick = event.target.closest('button');
     if(!cellClick) {
-        return
-    }
+        return;
+    };
     const cellNumber = Number(cellClick.innerText);
     const emptyCell = emtyNumber;
     const cellCoord = findCoordinates(cellNumber, matrix);
     const emptyCellCoord = findCoordinates(emptyCell, matrix);
     const canMove = canMoveCell(cellCoord, emptyCellCoord);
     if(canMove) {
-        move(cellCoord, emptyCellCoord, matrix)
-    }
-})
+        move(cellCoord, emptyCellCoord, matrix);
+        count++;
+    };
+});
 
 function findCoordinates(number, matrix) {
     for (let y = 0; y < matrix.length; y++) {
         for (let x = 0; x < matrix.length; x++) {
             if(matrix[y][x] === number) {
-                return {x, y}
-            }
-        }
-    }
-    return null
-}
+                return {x, y};
+            };
+        };
+    };
+    return null;
+};
 
 function canMoveCell(coord1, coord2) {
     return ((Math.abs(coord1.x - coord2.x) === 1 && coord1.y === coord2.y || Math.abs(coord1.y - coord2.y) === 1 && coord1.x === coord2.x)) ? true : false;
@@ -146,7 +148,47 @@ function move(coord1, coord2, matrix) {
     matrix[coord1.y][coord1.x] = matrix[coord2.y][coord2.x];
     matrix[coord2.y][coord2.x] = storage;
     setPozitionMatrix(matrix);
-}
+};
+
+//-------------смещение позиции по нажатию на клавиатуру----------
+
+window.addEventListener('keydown', (event) => {
+    
+    if (!event.key.includes('Arrow')) {
+        return;
+    };
+
+    const emptyCellCoord = findCoordinates(emtyNumber, matrix);
+
+    const cellCoord = {
+        x: emptyCellCoord.x,
+        y: emptyCellCoord.y,
+    };
+
+    const direction = event.key.split('Arrow')[1].toLowerCase();
+
+    switch (direction) {
+        case 'up':
+            cellCoord.y +=1;
+            break;
+        case 'down':
+            cellCoord.y -=1;
+            break;
+        case 'left':
+            cellCoord.x +=1;
+            break;
+        case 'right':
+            cellCoord.x -=1;
+            break;
+    }
+
+    if(cellCoord.x >= matrix.length || cellCoord.x < 0 || cellCoord.y >= matrix.length || cellCoord.y < 0) {
+        return
+    }
+
+    move(cellCoord, emptyCellCoord, matrix);
+    count++;
+});
 
 
 // window.addEventListener('load', (e) => {
